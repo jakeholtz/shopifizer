6caf7132593b65fabd447f53174a23c3070e7f8f
Jake Holtz <jahholtz@gmail.com>
2019-04-24 16:35:27 -0700

    Cleaned up code

--- server.js
+++ server.js
@@ -9,41 +9,51 @@ app.use(express.static(__dirname + '/client'));
 /* Shopify credentials from .env variables */
 const { parsed } = require('dotenv').config();
 const { SHOPIFY_STORE_NAME, SHOPIFY_API_KEY, SHOPIFY_PASSWORD } = parsed;
-const SHOPIFY_API_URL = `https://${SHOPIFY_API_KEY}:${SHOPIFY_PASSWORD}@${SHOPIFY_STORE_NAME}.myshopify.com/admin/products.json`;
+const SHOPIFY_API_URL = `https://${SHOPIFY_API_KEY}:${SHOPIFY_PASSWORD}@${SHOPIFY_STORE_NAME}.myshopify.com/admin`;
 
 /* Main page */
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'client/views/index.html'));
 });
 
 /* Sends product information from Shopify */
 app.get('/products', (req, res) => {
-  request(SHOPIFY_API_URL, (error, response, data) => {
+  request(`${SHOPIFY_API_URL}/products.json`, (error, response, data) => {
     let shopifyData = parseShopifyJSON(data);
     res.send(shopifyData);
   });
 });
 
+app.delete('/products', (req, res) => {
+
+  let { id, product_id } = req.query;
+  request.delete(`${SHOPIFY_API_URL}/api/products/#{${product_id}}.json`, (error, response, data) => {
+    if (error) {
+      console.error(error)
+    } else {
+      res.send('Success deleting item!');
+    } 
+  });
+});
+
+
 /* Function to parse data from Shopify */
 function parseShopifyJSON(data) {
-
   data = JSON.parse(data);
   let items = [];
 
   data.products.forEach(item => {
-    let { title, variants, inventory_quantity, sku, created_at, updated_at, price = null } = item;
+    let { title, variants } = item;
+    variants.forEach(variantItem => {
+      
+      let { id, product_id, title: variant, inventory_quantity, sku, created_at, updated_at, price } = variantItem;
+      let hasVariants = variant === 'Default Title' ? false : true;
+      if(!hasVariants) variant = null;
 
-    if (!variants) {
-      items.push({ description: title, title, variant: null, quantity: inventory_quantity, sku, created_at, updated_at, price });
-    } else {
-      variants.forEach(variantItem => {
-        
-        let { title: variant, inventory_quantity, sku, created_at, updated_at, price } = variantItem;
-        let description = `${title} (${variant})`;
+      let description = hasVariants ? `${title} (${variant})` : title;
 
-        items.push({ description, title, variant, quantity: inventory_quantity, sku, created_at, updated_at, price });
-      });
-    }
+      items.push({ id, product_id, description, title, variant, quantity: inventory_quantity, sku, created_at, updated_at, price });
+    });
   })
 
   return items;
