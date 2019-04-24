b5c7cdefbc5cc1dbbc2cf42f6e2ab57b203a5c55
Jake Holtz <jahholtz@gmail.com>
2019-04-23 20:17:46 -0700

    Implemented date filter

--- app/index.js
+++ app/index.js
@@ -1,15 +1,42 @@
-const stitchLite = angular.module('StitchFront',[]);
+const stitchFront = angular.module('StitchFront',[]);
 
-stitchLite.controller('MainController', ['$scope', '$http', function($scope, $http) {
-  
-  $scope.products = [];
+stitchFront.controller('MainController', mainController);
 
-  $http({ method: 'GET', url: '/products'})
-    .then((res) => {
-      $scope.products = res.data;
-    }, (res, err) => {
-      if (err) throw err;
-    }
-  );
+function mainController($http) {
 
-}]);
+  const ctrl = this;
+
+  ctrl.products = [];
+  ctrl.filteredProducts = [];
+  ctrl.filters = {};
+
+  ctrl.$onInit = () => {
+    $http.get('/products', { cache: true })
+      .then((res) => initiateProducts(res.data))
+      .catch(err => console.error(err));
+  };
+
+  ctrl.filterProducts = () => {
+    ctrl.filteredProducts = ctrl.products.filter((product) => {
+      let { name, updated_at, variant = '' } = product;
+      let { searchString, startDate, endDate } = ctrl.filters;
+      let productString = (`${name} ${variant}`).toLowerCase();
+
+      let hasString = searchString ? productString.includes(searchString) : true;
+      let greaterThanStart = startDate ? sanitizeDate(updated_at) >= sanitizeDate(startDate) : true;
+      let lessThanEnd = endDate ? sanitizeDate(updated_at) <= sanitizeDate(endDate) : true;
+
+      return hasString && greaterThanStart && lessThanEnd;
+    });
+  }
+
+  function initiateProducts(products) {
+    ctrl.products = products;
+    ctrl.filteredProducts = products;
+  }
+
+  function sanitizeDate(date) {
+    if (!(date instanceof Date)) date = new Date(date);
+    return date.setHours(0,0,0,0).valueOf();
+  }
+};
